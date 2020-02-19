9dd49a50ccffa33e6a1004d00c4ccc5c89a65a98
Michael Kilesa <kilesa@gmail.com>
2020-02-19 15:07:43 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+export const option = {
+  empty: '0',
+  external: '1',
+  internal: '2',
+}
+
+export const internal = 'Some internal data';
+
 function App() {
+
+  const [selected, setSelected] = useState(option.empty);
+  const [external, setExternal] = useState('');
+
+  useEffect(() => {
+    if (selected === option.external) {
+      setExternal('Loading...');
+      try {
+        fetch('https://api.kanye.rest')
+          .then(res => res.json())
+          .then(data => {
+            setExternal(data.quote);
+          });
+      } catch (error) {
+        setExternal(error.toString());
+      }
+    }
+  }, [selected]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <form className="App App-header">
+
+      <select onChange={e => setSelected(e.target.value)} value={selected} data-testid="select">
+        <option value={option.empty}>empty</option>
+        <option value={option.external}>external</option>
+        <option value={option.internal}>internal</option>
+      </select>
+
+      {selected === option.external
+        && <textarea name="external" id="ta" cols="30" rows="10" value={external} readOnly data-testid="textarea"></textarea>}
+
+      {selected === option.internal
+        && <input type="text" defaultValue={internal} data-testid="input" />}
+
+    </form>
+
   );
+
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent, waitForDomChange } from '@testing-library/react';
+import fetchMock from 'fetch-mock';
+import App, { option, internal } from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('app component', async () => {
+
+  fetchMock.restore().mock(/https:\/\/api.kanye.rest/, {
+    body: { quote: 'Whatever' }
+  });
+
+  const { getByTestId } = render(<App />);
+  const select = getByTestId('select');
+  expect(select).toBeInTheDocument();
+
+  fireEvent.change(select, { target: { value: option.external } });
+  const textarea = getByTestId('textarea');
+  expect(textarea).toBeInTheDocument();
+  expect(textarea.textContent).toBe('Loading...');
+  await waitForDomChange();
+  expect(textarea.textContent).toBe('Whatever');
+
+  fireEvent.change(select, { target: { value: option.internal } });
+  const input = getByTestId('input');
+  expect(textarea).not.toBeInTheDocument();
+  expect(input).toBeInTheDocument();
+  expect(input.value).toBe(internal);
+
+  fireEvent.change(select, { target: { value: option.empty } });
+  expect(textarea).not.toBeInTheDocument();
+  expect(input).not.toBeInTheDocument();
+
 });
